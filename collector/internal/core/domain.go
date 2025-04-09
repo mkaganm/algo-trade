f6313c7df69ee72b9b8385ef6723883f0108800c
mkaganm <mustafakaganmeric@gmail.com>
2025-04-09 17:53:17 +0300

    update collector

--- collector/internal/core/domain.go
+++ collector/internal/core/domain.go
@@ -6,8 +6,18 @@ import (
 )
 
 type OrderBookUpdate struct {
-	Data      string    `bson:"data"`
-	Timestamp time.Time `bson:"timestamp"`
+	Data      OrderBookData `bson:"data"`
+	Timestamp time.Time     `bson:"timestamp"`
+}
+
+type OrderBookData struct {
+	EventType     string     `json:"e"` // "depthUpdate"
+	EventTime     int64      `json:"E"` // Event timestamp
+	Symbol        string     `json:"s"` // "BTCUSDT"
+	FirstUpdateID int64      `json:"U"` // First update ID in event
+	FinalUpdateID int64      `json:"u"` // Final update ID in event
+	BidUpdates    [][]string `json:"b"` // [["Price", "Quantity"],...]
+	AskUpdates    [][]string `json:"a"` // [["Price", "Quantity"],...]
 }
 
 type OrderBookRepository interface {
